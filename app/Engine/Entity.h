3352a16e7feb9d8634c6b99ebc1f954f66f4a1f7
Zeitgeist <zeit_geist@posteo.de>
2023-10-15 18:15:13 +0200

    Engine: Add abstract Entity base class.
    This class offers and maintains some rudimentary SDL
    capabilities. Furthermore, it can be extended by passing
    corresponding flags upon construction, initializing
    components via a built-in factory method.

---
+++ app/Engine/Entity.h
@@ -0,0 +1,63 @@
+//
+// Created by zeitgeist on 10.10.23.
+//
+#pragma once
+
+#include "Collideable.h"
+#include "Drawable.h"
+#include "Moveable.h"
+
+#include <SDL2/SDL.h>
+
+#include <memory>
+
+class SDL_Renderer;
+
+class Entity {
+public:
+    struct Values {
+        int x{-1};
+        int y{-1};
+        int width{0};
+        int height{0};
+        float velocity{0.f};
+        SDL_Color color{0x00, 0x00, 0x00, 0x00};
+    };
+
+    using ComposeMask = uint8_t;
+    enum Compose : ComposeMask {
+        DRAWABLE = 1 << 0,
+        MOVEABLE = 1 << 1,
+        COLLIDEABLE = 1 << 1,
+    };
+
+    virtual ~Entity() = default;
+    virtual void update(float delta) = 0;
+
+    virtual void render(SDL_Renderer& renderer);
+    virtual void init(Values v);
+    [[nodiscard]] int getX() const;
+
+    [[nodiscard]] int getY() const;
+    [[nodiscard]] int getWidth() const;
+    [[nodiscard]] int getHeight() const;
+    [[nodiscard]] const SDL_Rect& getRect() const;
+    virtual void setX(int x);
+
+    virtual void setY(int y);
+    virtual void setWidth(int width);
+    virtual void setHeight(int height);
+
+    [[nodiscard]] virtual Collideable* getCollideable() const;
+    [[nodiscard]] virtual Drawable* getDrawable() const;
+    [[nodiscard]] virtual Moveable* getMoveable() const;
+
+protected:
+    explicit Entity(ComposeMask composeMask);
+
+    SDL_Rect m_rect;
+
+    std::unique_ptr<Collideable> m_collideable;
+    std::unique_ptr<Moveable> m_moveable;
+    std::unique_ptr<Drawable> m_drawable;
+};
---
+++ app/Engine/src/Entity.cpp
@@ -0,0 +1,102 @@
+//
+// Created by zeitgeist on 10.10.23.
+//
+
+#include "Engine/Entity.h"
+
+Entity::Entity(ComposeMask composeMask)
+    : m_rect({0, 0, 0, 0}) {
+    auto& self = *this;
+    if (composeMask & DRAWABLE) {
+        m_drawable = std::make_unique<Drawable>(self);
+    }
+    if (composeMask & MOVEABLE) {
+        m_moveable = std::make_unique<Moveable>(self);
+    }
+    if (composeMask & COLLIDEABLE) {
+        m_collideable = std::make_unique<Collideable>(self);
+    }
+}
+
+void Entity::init(Entity::Values v) {
+    setX(v.x);
+    setY(v.y);
+    setWidth(v.width);
+    setHeight(v.height);
+    if (m_drawable) {
+        m_drawable->setColor(v.color);
+    }
+    if (m_moveable) {
+        m_moveable->setVelocity(v.velocity);
+    }
+}
+
+void Entity::render(SDL_Renderer& renderer) {
+    if (m_drawable) {
+        m_drawable->render(renderer);
+    }
+}
+
+int Entity::getX() const {
+    return m_rect.x;
+}
+
+int Entity::getY() const {
+    return m_rect.y;
+}
+
+int Entity::getWidth() const {
+    return m_rect.w;
+}
+
+int Entity::getHeight() const {
+    return m_rect.h;
+}
+
+const SDL_Rect& Entity::getRect() const {
+    return m_rect;
+}
+
+void Entity::setX(int x) {
+    if (m_collideable) {
+        m_collideable->setCenterX(getX() - x);
+    }
+    m_rect.x = static_cast<Sint16>(x);
+}
+
+void Entity::setY(int y) {
+    if (m_collideable) {
+        m_collideable->setCenterY(getY() - y);
+    }
+    m_rect.y = static_cast<Sint16>(y);
+}
+
+void Entity::setWidth(int width) {
+    if (m_collideable) {
+        const auto extentX = width / 2;
+        m_collideable->setExtentX(extentX);
+        m_collideable->setCenterX(getX() + extentX);
+    }
+    m_rect.w = static_cast<Uint16>(width);
+}
+
+void Entity::setHeight(int height) {
+    if (m_collideable) {
+        const auto extentY = height / 2;
+        m_collideable->setExtentY(extentY);
+        m_collideable->setCenterY(getY() + extentY);
+    }
+    m_rect.h = static_cast<Uint16>(height);
+}
+
+Collideable* Entity::getCollideable() const {
+    return m_collideable.get();
+}
+
+Drawable* Entity::getDrawable() const {
+    return m_drawable.get();
+}
+
+Moveable* Entity::getMoveable() const {
+    return m_moveable.get();
+}

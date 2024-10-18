4037a3424c086cd3b00313aba68d670af8c6a778
Zeitgeist <zeit_geist@posteo.de>
2024-10-18 11:41:49 +0200

    Add dedicated game events

---
+++ app/Game/include/Game/GameEvent.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Engine/Event.h>
+#include <Engine/Entity.h>
+
+class Brick;
+
+namespace events {
+
+    struct BrickDestroyedEvent : Event {
+        explicit BrickDestroyedEvent(const Brick& brick) : brick(brick) {}
+        const Brick& brick;
+    };
+
+}
+

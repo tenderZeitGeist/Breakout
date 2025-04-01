0662218b093d5ac00d1fa201865e4904f80287ba
Zeitgeist <zeit_geist@posteo.de>
2025-04-01 17:43:28 +0200

    Start implementing Score class

---
+++ app/Game/include/Game/Score.h
@@ -0,0 +1,24 @@
+
+#include <Engine/Digit.h>
+
+#include <array>
+
+class Score : public Entity {
+    static constexpr std::size_t kNumOfDigits{3};
+
+public:
+    void setScore(int score);
+    [[nodiscard]] int getScore() const;
+
+    void setBlinking(bool blinking);
+
+    void render(SDL_Renderer& renderer) override;
+    void update(float delta) override;
+
+private:
+    std::array<Digit, kNumOfDigits> m_digits;
+    int m_score{0};
+    bool m_blinking{false};
+    std::size_t m_blinkCounter{0};
+    std::size_t m_blinksLeft{0};
+};

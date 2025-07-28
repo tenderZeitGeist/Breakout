be39564bb46e36cb3238b219c047091f93713337
Zeitgeist <zeit_geist@posteo.de>
2025-07-28 15:02:22 +0200

    Score: Override reset function & change interface for setting score

--- app/Game/include/Game/Score.h
+++ app/Game/include/Game/Score.h
@@ -10,16 +10,18 @@ public:
     static constexpr std::size_t kNumOfDigits{3};
 
     explicit Score();
-    void setScore(int score);
-    [[nodiscard]] int getScore() const;
-
+    void increaseScore(int points);
     void setBlinking(bool blinking);
 
     void init(Values v) override;
     void render(SDL_Renderer& renderer) override;
-    void update(float delta) override;
+    void update(float) override;
+    void reset() override;
 
 private:
+    void setScore(int score);
+    [[nodiscard]] int getScore() const;
+
     std::array<Digit, kNumOfDigits> m_digits;
     int m_score{0};
     bool m_blinking{false};

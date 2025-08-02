f5ee4556b5ea6a4084e1a2ea557c7a304b8a946c
Zeitgeist <zeit_geist@posteo.de>
2025-08-02 12:40:57 +0200

    Engine: Add utility class for measuring performance

---
+++ app/Engine/include/Engine/PerformanceMetrics.h
@@ -0,0 +1,24 @@
+
+#include <chrono>
+#include <string_view>
+#include <unordered_map>
+
+class PerformanceMetrics {
+    using clock = std::chrono::high_resolution_clock;
+
+    struct Measurements {
+        clock::time_point start;
+        std::vector<double> timings;
+    };
+
+public:
+    ~PerformanceMetrics();
+    void start(std::string_view identifier);
+    void stop(std::string_view identifier);
+    void writeResults(std::string_view fileName);
+    void print();
+    void reset();
+
+private:
+    std::unordered_map<std::string_view, Measurements> m_measurements;
+};

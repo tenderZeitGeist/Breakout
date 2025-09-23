9bfaf7ad3ea266766a1dfe2eecdcc9d178525e74
tenderZeitGeist <zeit_geist@posteo.de>
2025-09-24 00:46:57 +0200

    Entity: Consolidate postion logic & create additional shapes for rects & circles

--- app/Engine/include/Engine/Entity.h
+++ app/Engine/include/Engine/Entity.h
@@ -6,6 +6,7 @@
 #include "Collideable.h"
 #include "Drawable.h"
 #include "Moveable.h"
+#include "Shape.h"
 
 #include <SDL2/SDL.h>
 
@@ -27,6 +28,7 @@ public:
         int height{0};
         float velocity{0.f};
         SDL_Color color{0x00, 0x00, 0x00, 0x00};
+        ShapeType shape{ShapeType::RECT};
     };
 
     using ComposeMask = uint8_t;
@@ -47,33 +49,43 @@ public:
     [[nodiscard]] int getY() const;
     [[nodiscard]] int getWidth() const;
     [[nodiscard]] int getHeight() const;
+    [[nodiscard]] int getCenterX() const;
+    [[nodiscard]] int getCenterY() const;
+    [[nodiscard]] int getExtentX() const;
+    [[nodiscard]] int getExtentY() const;
     [[nodiscard]] const SDL_Rect& getRect() const;
 
     virtual void setX(int x);
     virtual void setY(int y);
     virtual void setWidth(int width);
     virtual void setHeight(int height);
+    virtual void setCenterX(int centerX);
+    virtual void setCenterY(int centerY);
+    virtual void setExtentX(int extentX);
+    virtual void setExtentY(int extentY);
 
     [[nodiscard]] virtual std::reference_wrapper<const Collideable> getCollideable() const;
     [[nodiscard]] virtual std::reference_wrapper<const Drawable> getDrawable() const;
     [[nodiscard]] virtual std::reference_wrapper<const Moveable> getMoveable() const;
+    [[nodiscard]] virtual std::reference_wrapper<const ShapeVariant> getShape() const;
     [[nodiscard]] virtual std::reference_wrapper<Collideable> getCollideable();
     [[nodiscard]] virtual std::reference_wrapper<Drawable> getDrawable();
     [[nodiscard]] virtual std::reference_wrapper<Moveable> getMoveable();
+    [[nodiscard]] virtual std::reference_wrapper<ShapeVariant> getShape();
 
     virtual void onDebug(bool debug);
 
 protected:
     explicit Entity(ComposeMask composeMask);
-    Entity(Entity&&) = default;
-    Entity& operator=(Entity&&) = default;
+
+    int m_centerX{0};
+    int m_centerY{0};
+    int m_extentX{0};
+    int m_extentY{0};
 
     SDL_Rect m_rect;
-
     Drawable m_drawable;
     Moveable m_moveable;
     Collideable m_collideable;
+    ShapeVariant m_shapeVariant;
 };
-
-class Circle : public Entity {};
-class Rect : public Entity {};
--- app/Engine/include/Engine/Shape.h
+++ app/Engine/include/Engine/Shape.h
@@ -4,35 +4,51 @@
 #include <SDL2/SDL_rect.h>
 
 #include <functional>
+#include <variant>
 
 class Entity;
 class RectShape;
+class CircleShape;
 
+using ShapeVariant = std::variant<RectShape, CircleShape>;
+enum class ShapeType { RECT, CIRCLE };
+
+template<typename Derived>
 class Shape {
 public:
-    virtual ~Shape() = default;
-    explicit Shape(std::reference_wrapper<Entity> entity);
-    [[nodiscard]] virtual SDL_Rect getBoundingBox() = 0;
-    [[nodiscard]] virtual std::reference_wrapper<const Entity> getEntity() const;
+
+    explicit Shape(std::reference_wrapper<Entity> entity)
+        : m_entity(entity) {
+    }
+
+    [[nodiscard]] const SDL_Rect& getBoundingBox() const {
+        return static_cast<const Derived*>(this)->getBoundingBox();
+    }
+
+    [[nodiscard]] std::reference_wrapper<const Entity> getEntity() const {
+        return m_entity;
+    }
 
 protected:
     std::reference_wrapper<Entity> m_entity;
 };
 
-class CircleShape : public Shape {
+class CircleShape : public Shape<CircleShape> {
 public:
     explicit CircleShape(int radius, std::reference_wrapper<Entity> entity);
     [[nodiscard]] bool collidesWith(const CircleShape& other) const;
     [[nodiscard]] bool collidesWith(const RectShape& other) const;
-    [[nodiscard]] SDL_Rect getBoundingBox() override;
+    [[nodiscard]] const SDL_Rect& getBoundingBox() const;
+    [[nodiscard]] int getRadius() const;
+
 private:
     int m_radius;
 };
 
-class RectShape : public Shape {
+class RectShape : public Shape<RectShape> {
 public:
     explicit RectShape(std::reference_wrapper<Entity> entity);
     [[nodiscard]] bool collidesWith(const CircleShape& other) const;
     [[nodiscard]] bool collidesWith(const RectShape& other) const;
-    [[nodiscard]] SDL_Rect getBoundingBox() override;
+    [[nodiscard]] const SDL_Rect& getBoundingBox() const;
 };

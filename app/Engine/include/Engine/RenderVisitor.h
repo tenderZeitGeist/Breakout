07f00ea6692d635956cf973285004142e52c3915
tenderZeitGeist <zeit_geist@posteo.de>
2025-09-24 00:47:56 +0200

    Add RenderVisitor to project

---
+++ app/Engine/include/Engine/RenderVisitor.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "Shape.h"
+#include "SDL2/SDL_render.h"
+
+struct RenderVisitor {
+    explicit RenderVisitor(SDL_Renderer& renderer);
+    void operator()(const RectShape& shape) const;
+    void operator()(const CircleShape& shape) const;
+    SDL_Renderer& m_renderer;
+};
---
+++ app/Engine/src/RenderVisitor.cpp
@@ -0,0 +1,53 @@
+
+#include <Engine/RenderVisitor.h>
+
+#include "Engine/Entity.h"
+
+#include <cmath>
+#include <numbers>
+
+RenderVisitor::RenderVisitor(SDL_Renderer& renderer)
+    : m_renderer(renderer) {
+}
+
+
+void RenderVisitor::operator()(const RectShape& shape) const {
+    SDL_RenderFillRect(&m_renderer, &shape.getBoundingBox());
+}
+
+void RenderVisitor::operator()(const CircleShape& shape) const {
+    const auto radius = shape.getRadius();
+    const auto diameter = radius * 2;
+    const auto centerX = shape.getEntity().get().getCenterX();
+    const auto centerY = shape.getEntity().get().getCenterY();
+
+    int x = radius - 1;
+    int y = 0;
+    int dx = 1;
+    int dy = 1;
+    int error = dx - diameter;
+
+    std::array<SDL_Point, 8> points{};
+    while (x >= y) {
+        points = {{{centerX + x, centerY - y},
+                   {centerX + x, centerY + y},
+                   {centerX - x, centerY - y},
+                   {centerX - x, centerY + y},
+                   {centerX + y, centerY - x},
+                   {centerX + y, centerY + x},
+                   {centerX - y, centerY - x},
+                   {centerX - y, centerY + x}}};
+        SDL_RenderDrawLines(&m_renderer, points.data(), points.size());
+
+        if (error <= 0) {
+            ++y;
+            error += dy;
+            dy += 2;
+        }
+        if (error > 0) {
+            --x;
+            dx += 2;
+            error += dx - diameter;
+        }
+    }
+}

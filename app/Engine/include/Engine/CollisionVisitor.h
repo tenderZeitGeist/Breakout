3ea1114844e166603e45dd6b0111bf88d5384de2
tenderZeitGeist <zeit_geist@posteo.de>
2025-09-24 00:47:27 +0200

    Add CollisionVisitor to project

---
+++ app/Engine/include/Engine/CollisionVisitor.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <variant>
+
+class CircleShape;
+class RectShape;
+
+struct CollisionVisitor {
+    [[nodiscard]] bool operator()(const CircleShape& lhs, const CircleShape& rhs) const;
+    [[nodiscard]] bool operator()(const CircleShape& lhs, const RectShape& rhs) const;
+    [[nodiscard]] bool operator()(const RectShape& lhs, const CircleShape& rhs) const;
+    [[nodiscard]] bool operator()(const RectShape& lhs, const RectShape& rhs) const;
+};
+
---
+++ app/Engine/src/CollisionVisitor.cpp
@@ -0,0 +1,19 @@
+
+#include <Engine/CollisionVisitor.h>
+#include <Engine/Shape.h>
+
+bool CollisionVisitor::operator()(const CircleShape& lhs, const CircleShape& rhs) const {
+    return lhs.collidesWith(rhs);
+}
+
+bool CollisionVisitor::operator()(const CircleShape& lhs, const RectShape& rhs) const {
+    return lhs.collidesWith(rhs);
+}
+
+bool CollisionVisitor::operator()(const RectShape& lhs, const CircleShape& rhs) const {
+    return lhs.collidesWith(rhs);
+}
+
+bool CollisionVisitor::operator()(const RectShape& lhs, const RectShape& rhs) const {
+    return lhs.collidesWith(rhs);
+}
